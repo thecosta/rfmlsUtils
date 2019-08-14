11d44636a8c5a8964aa595a70317fd4715ea4bf4
bruno <bru.costa.bc@gmail.com>
2019-08-13 21:31:45 -0400

    restructured getting fir ensmble results

--- fir/get_fir_ensemble_acc.py
+++ fir/get_fir_ensemble_acc.py
@@ -4,79 +4,128 @@ import pickle
 import numpy as np
 
 
-device_paths = glob.glob('/home/bruno/RFMLS/results/fir/1C/wifi/raw/baseline/firinit_ensemble/*')
+device_paths = glob.glob('/scratch/bruno/RFMLS/results/fir/1C/wifi/raw/baseline/firinit_ensemble_5taps_probsum/*')
 
-ensemble = {}
+example_acc = {}
 
-# Iterate over device paths
+firmodel_acc = []
+firmodel_acc_ensemble = {}
+
+device_acc = []
+device_acc_ensemble = {}
+
+total_devices = 0
+correct_devices = 0
+
+# Iterate over device classes
 for device_path in device_paths:
 
-    pred_files = glob.glob(device_path + '/*/1C_raw_wifi/preds.pkl')    
-    tot_prob = np.array([50,])
-    # Iterate over all predictions of weights tested on device
-    for pred_file in pred_files:
+    fir_models = glob.glob(device_path + '/*/1C_raw_wifi/preds.pkl')    
+    total_device_prob = np.array([50,])
+    total_device_examples = 0
+    correct_device_examples = 0
+    total_devices += 1
+    
+    # Iterate over all FIR models
+    for fir_model in fir_models:
 
-        #print(preds)
-        device_n = int(pred_file.split('/')[-4])
-        weight_n = int(pred_file.split('/')[-3])
+        device_n = int(fir_model.split('/')[-4])
+        weight_n = int(fir_model.split('/')[-3])
 
-        real_label = device_n # before this was weight_n
+        total_fir_prob = np.array([50,])
+        total_fir_examples = 0
+        correct_fir_examples = 0
+    
+        real_label = device_n
 
-        total_examples = 0
-        correct_examples = 0
-        #tot_prob = np.array([50,])
-        with open(pred_file, 'r') as f:
+        with open(fir_model, 'r') as f:
             preds_pkl = pickle.load(f)
-            device_acc = 0
 
+            # Iterate over all examples
             for pred in preds_pkl['preds_exp']:
 
-                pred = preds_pkl['preds_exp'][pred]
-                tot_prob = tot_prob+pred
-                '''
-                total_examples += 1
-                predicted_class = np.argmax(pred)
-                #print 'device: ', device_n, ', ', 'weight: ', weight_n, 'predicted: ', predicted_class
-                #print(pred)
-                #if total_examples == 100:
-                #    a
+                predictions = preds_pkl['preds_exp'][pred]
+                total_fir_prob = total_fir_prob + predictions
+                total_device_prob = total_device_prob + predictions
 
-                if predicted_class == int(real_label) and predicted_class == device_n :
-                    #print 'device: ', device_n, ', ', 'weight: ', weight_n, 'predicted: ', predicted_class
-                    correct_examples += 1
-                
+                total_fir_examples += 1
+                total_device_examples += 1
 
-            acc = correct_examples*1.0 / total_examples
+                predicted_example = np.argmax(predictions)
 
-            if device_n not in ensemble:
-                ensemble[device_n] = [(weight_n, acc)]
+                if device_n == predicted_example:
+                    correct_fir_examples += 1
+                    correct_device_examples += 1
+        
+        # FIR argmax of each example
+        firmodel_acc.append((weight_n, correct_fir_examples, total_fir_examples))
+
+        # FIR argmax of all prob vectors
+        if np.argmax(total_fir_prob) == device_n:
+            if device_n in firmodel_acc_ensemble:
+                firmodel_acc_ensemble[device_n] += 1
             else:
-                ensemble[device_n].append((weight_n, acc))
-            '''
-    predicted_class = np.argmax(tot_prob)
-    if device_n == 0:
-        print tot_prob
-    ensemble[device_n] = predicted_class
+                firmodel_acc_ensemble[device_n] = 1
+        
+        # End of FIR model loop
+                                
+    d_acc = 1.0*correct_device_examples / total_device_examples
+    device_acc.append((device_n, d_acc))
 
-print ensemble
+    if np.argmax(total_device_prob) == device_n:
+        device_acc_ensemble[device_n] = 1
+    else:
+        device_acc_ensemble[device_n] = 0
 
-#a
-'''
-n = np.array(ensemble[6], dtype=[('device', int), ('acc', float)])
-n = np.sort(n, order='device')
 
-correct = 0
-for device_class in ensemble:
-    highest_class = 0
-    highest_prob = 0
-    for t in ensemble[device_class]:
-        if t[1] > highest_prob:
-            highest_class = t[0]
-            highest_prob = t[1]
+for key in firmodel_acc_ensemble:
+    firmodel_acc_ensemble[key] = 1.0*firmodel_acc_ensemble[key]/50
 
-    #print device_class, ', ', highest_class
-    if int(device_class) == int(highest_class):
-        correct += 1
 
-print 'Correct classes: ', 1.0*correct/50
-'''
+# Get final results for FIR examples
+tmp = []
+for n in range(total_devices):
+    correct = 0
+    total = 0
+    for t in firmodel_acc:
+        if t[0] == n:
+            correct += t[1]
+            total += t[2]
+    tmp.append((n, 1.0*correct/total))    
+firmodel_acc = tmp
+
+firmodel_acc = np.array(firmodel_acc, dtype=[('device', int), ('acc', float)])
+firmodel_acc = np.flip(np.sort(firmodel_acc, order='acc'))
+order = []
+for t in firmodel_acc:
+    order.append(t[0])
+    print t[1]
+
+# Get final results for FIR prob vector
+tmp = []
+for n in order:
+    for t in firmodel_acc_ensemble:
+        if t == n:
+            tmp.append((n, firmodel_acc_ensemble[t]))    
+
+firmodel_acc_ensemble = tmp
+
+
+device_acc = np.array(device_acc, dtype=[('device', int), ('acc', float)])
+device_acc = np.flip(np.sort(device_acc, order='acc'))
+order = []
+for t in device_acc:
+    order.append(t[0])
+    #print t[1]
+
+
+
+#for t in firmodel_acc_ensemble:
+#    print t[1]
+#print m
+
+#print 'Per FIR ex:\n',  firmodel_acc, '\n'
+#print 'Per FIR accuracies ensemble:\n', firmodel_acc_ensemble, '\n'
+#print 'Per device accuries:\n', device_acc, '\n'
+#print 'Per device accuracies ensemble:\n', device_acc_ensemble, '\n'
+
